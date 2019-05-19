3a164090fa0ad39d339b8f0134f2ddf9e46e616f
Administrator <yuexiangbao@yeah.net>
2019-05-19 17:13:04 +0800

    help models add class Meta

--- help/models.py
+++ help/models.py
@@ -7,3 +7,10 @@ class Help(models.Model):
     title = models.CharField(max_length=50, verbose_name="文章标题")  # 文章标题
     order_by = models.PositiveIntegerField(verbose_name="序号")    # 用于排序
     contents = models.TextField(blank=True)                       #  文章内容
+
+    class Meta:
+        ordering = ('order_by',)    # 根据order_by排序
+        verbose_name = '文章'
+
+    def __str__(self):
+        return self.title
