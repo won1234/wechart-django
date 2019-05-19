15c6fddbe36f1dff15dadf468f7a315d6b2582bd
Administrator <yuexiangbao@yeah.net>
2019-05-19 17:10:24 +0800

    change help models

--- help/models.py
+++ help/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+# 发表帮助文档
+class Help(models.Model):
+    title = models.CharField(max_length=50, verbose_name="文章标题")  # 文章标题
+    order_by = models.PositiveIntegerField(verbose_name="序号")    # 用于排序
+    contents = models.TextField(blank=True)                       #  文章内容
