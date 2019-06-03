8b624e0993ce75c437ff663090ca5444bb42a0da
Administrator <yuexiangbao@yeah.net>
2019-06-03 14:00:07 +0800

    add help admin.py

--- help/admin.py
+++ help/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Help
+
 
 # Register your models here.
+
+class HelpAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order_by', 'contents']
+
+
+admin.site.register(Help, HelpAdmin)
--- help/models.py
+++ help/models.py
@@ -5,11 +5,11 @@ from django.db import models
 # 发表帮助文档
 class Help(models.Model):
     title = models.CharField(max_length=50, verbose_name="文章标题")  # 文章标题
-    order_by = models.PositiveIntegerField(verbose_name="序号")    # 用于排序
-    contents = models.TextField(blank=True)                       #  文章内容
+    order_by = models.PositiveIntegerField(verbose_name="序号")  # 用于排序
+    contents = models.TextField(blank=True)  # 文章内容
 
     class Meta:
-        ordering = ('order_by',)    # 根据order_by排序
+        ordering = ('order_by',)  # 根据order_by排序
         verbose_name = '文章'
 
     def __str__(self):
