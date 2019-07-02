cf241e85a90f61ecb833eaa68db7b1648c131165
Administrator <yuexiangbao@yeah.net>
2019-07-02 15:09:56 +0800

    add orders export to csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
+import csv
+import datetime
+from django.http import HttpResponse
+
+# 订单导出CSV
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta  # 用于获取model字段
+    response = HttpResponse(content_type='text/csv')  # 创建一个HttpResponse实例包含一个定制text/csv内容类型来告诉浏览器这个响应需要处理为一个CSV文件。
+    # 添加一个Content-Disposition头来指示这个HTTP响应包含一个附件。
+    response['Content-Disposition'] = 'attachment; \
+           filename={}.csv'.format(opts.verbose_name)   # 导出的文件名
+    writer = csv.writer(response)  # 创建一个CSV writer对象，该对象将会被写入response对象。
+    # 动态的获取model字段通过使用模型（moedl）_meta选项的get_fields()方法。我们排除多对多以及一对多的关系。
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information  添加一个头行包含models的字段名。
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows  写入数据
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)  # 反射，取得字段的值
+            if isinstance(value, datetime.datetime):   # 是否是时间对象
+                value = value.strftime('%Y-%m-%d %H:%M:%S')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = '导出CSV'  # 在模版中显示的名称
+
 
 # 使用 ModelInline 来把它引用为  OrderAdmin 类的内联元素。一个内联元素允许你在同一编辑页引用模型
 class OrderItemInline(admin.TabularInline):
@@ -10,9 +40,10 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'paid', 'send', 'created', 'updated']
-    list_filter = ['paid', 'send', 'created', 'user',  'updated']
+    list_filter = ['paid', 'send', 'created', 'user', 'updated']
     date_hierarchy = "created"
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
 
 admin.site.register(Order, OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -13,13 +13,13 @@ class Order(models.Model):
     # address = models.CharField(max_length=250)
     # postal_code = models.CharField(max_length=20)  # 邮政编码
     # city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    updated = models.DateTimeField(auto_now=True, verbose_name='修改时间')
     paid = models.BooleanField(default=False, verbose_name='是否支付')  # 区分支付和未支付
     send = models.BooleanField(default=False, verbose_name='是否发货')  # 是否发货
     is_delete = models.BooleanField(default=False, verbose_name='是否删除')  # FALSE时表示未删除，True表示改订单删除了。
     description = models.TextField(verbose_name='订单描述', blank=True, null=True)  # 订单描述,为管理员时使用
-    total_cost = models.CharField(max_length=20)    # 订单金额
+    total_cost = models.CharField(max_length=20, verbose_name='订单金额')    # 订单金额
 
     class Meta:
         ordering = ('-created',)   # 按创建时间倒叙排列
@@ -39,14 +39,16 @@ class Order(models.Model):
 # 保存物品，数量和每个物品的支付价格。
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', null=True,
-                              on_delete=models.SET_NULL)
+                              on_delete=models.SET_NULL,
+                              verbose_name='订单')
     product = models.ForeignKey(Product,
                                 related_name='order_items',
                                 null=True,
-                                on_delete=models.SET_NULL
+                                on_delete=models.SET_NULL,
+                                verbose_name='产品'
                                 )
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='单价')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='数量')
 
     def __str__(self):
         return '{}'.format(self.id)
